6d7a9dbbc41e385ae19c389c76a9af7e6456118f
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2026-01-09 20:51:09 +0200

    NODE JS

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// A simple console log message
-console.log('Hello, World!');
+// g:\_book\_development\javascript\js\Khan Yousaf - Advanced JavaScript Unleashed - 2024
+
+const arr = [1, 2, 3];
+
+console.log('--- for of ---');
+
+for (const n of arr) {
+    console.log(n);
+}
+
+console.log('--- for ---');
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log('--- iterator ---');
+
+const it = arr[Symbol.iterator]();
+let result = it.next();
+while (!result.done) {
+    console.log(result.value);
+    result = it.next();
+}
+
+console.log('---');
